d9d35b9e55305a56b2c917842d633826a622faf5
Dev Patel <devarshi.ap@gmail.com>
2023-03-25 14:32:52 -0400

    setup main

--- src/main/Market.java
+++ src/main/Market.java
@@ -1,49 +1,69 @@
 package main;
+
 import java.util.HashMap;
 import java.util.Scanner;
 
-import javax.lang.model.util.ElementScanner14;
-
-
 public class Market {
-    public static HashMap<String, Stock> stocks;
+    public static HashMap<String, Stock> stocks = new HashMap<>();
     private static int idCounter = 0;
 
     public static void main(String[] args) {
         // main program to run
+        Scanner scanner = new Scanner(System.in);
+
+        // prompt to run as Admin or Investor
+        String userInput;
+        do {
+            System.out.print("Run program as Admin (0) || Investor (1) : ");
+            userInput = scanner.nextLine();
+            if (!userInput.equals("0") && !userInput.equals("1"))
+                System.out.println("Try again!");
+        } while (!userInput.equals("0") && !userInput.equals("1"));
+
+        Investor investor;
+        Admin admin;
+
+        // prompt for User details
+        System.out.print("Enter name : ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter email : ");
+        String email = scanner.nextLine();
+
+        // invoke appropriate constructor on user
+        if (userInput.equals("0")) {
+            // ADMIN
+            System.out.println("You've chosen to play as Admin.");
+            
+            admin = new Admin(name, email);
+            
+            System.out.println("\n------+" + admin + "\n------+");
+        } else {
+            // INVESTOR
+            int initial_amount;
+            do {
+                System.out.print("Enter initial amount in wallet ($100,000,000 MAX) : ");
+                initial_amount = scanner.nextInt();
+                if (initial_amount > 100_000_000) System.out.println("Try again!");
+            } while (initial_amount > 100_000_000);
+            
+            investor = new Investor(name, email, initial_amount);
+            admin = new Admin("Bill gates", "bill@gmail.com"); // default dummy admin
+            admin.newInvestor(investor);
+            
+            System.out.println("\n------+" + investor + "\n------+");
+            System.out.println("\n------+" + admin + "\n------+");
+        }
+        
         
 
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Are you using this program as an Admin(0) or Investor(1)?\n> ");
-
-        String userInput = scanner.nextLine();
-
-        if (userInput == "0") {
-            Admin user;
-        }
-        else if (userInput == "1") {
-            Investor user;
-        }
-        else{
-            while (userInput != "0" | userInput != "1"){
-                scanner = new Scanner(System.in);
-                System.out.println("Please input the respective value for Admin/Investor access (0/1).\n> ");
-                userInput = scanner.nextLine();
-            }
-            if (userInput == "0") {
-                Admin user;
-            }
-            else if (userInput == "1") {
-                Investor user;
-            }
-        }
+        scanner.close();
     }
 
     /**
      * Return all stocks
      * 
-     * @return   A hash map of every stock 
+     * @return A hash map of every stock
      */
     public HashMap<String, Stock> getAllStocks() {
         return stocks;
@@ -52,9 +72,9 @@ public class Market {
     /**
      * Verify stock exists
      * 
-     * @param   sts Stock ticker symbol
+     * @param sts Stock ticker symbol
      * 
-     * @return   Boolean value corresponding to if the stock exists
+     * @return Boolean value corresponding to if the stock exists
      */
     public static boolean verifyStock(String sts) {
         return stocks.containsKey(sts);
@@ -72,38 +92,36 @@ public class Market {
      */
     public void manpage() {
         System.out.println(
-        """
-        Help Manual
-        -----------------------------------------------
-
-        exit
-        log
-        export
-        stock-price
-        new-stock
-        volume
-        buy
-        sell-stock
-        add-watch
-        del-watch
-        get-watch
-        get-portfolio
-        wallet
-        deposit
-        man
-        overwrite-watchlist
-        list-users
-        remove-user
-        list-stocks
-        
-        -----------------------------------------------
-        """);
+                """
+                        Manual Page
+                        -----------------------------------------------+
+                        exit\t\t\t<>
+                        log\t\t\t<>
+                        export\t\t\t<>
+                        stock-price\t\t<>
+                        new-stock\t\t<>
+                        volume\t\t\t<>
+                        buy\t\t\t<>
+                        sell-stock\t\t<>
+                        add-watch\t\t<>
+                        del-watch\t\t<>
+                        get-watch\t\t<>
+                        get-portfolio\t\t<>
+                        wallet\t\t\t<>
+                        deposit\t\t\t<>
+                        man\t\t\t<>
+                        overwrite-watchlist\t<>
+                        list-users\t\t<>
+                        remove-user\t\t<>
+                        list-stocks\t\t<>
+                        -----------------------------------------------+
+                        """);
     }
 
-     /**
+    /**
      * Generate ID for new user
      * 
-     * @return   Incremented ID 
+     * @return Incremented ID
      */
     public static int generateID() {
         return idCounter++;
