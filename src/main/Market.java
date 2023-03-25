9cfc5180008e89bb428ce24b511213a0e1b48ec7
Humza Khan <humzakhan7678@gmail.com>
2023-03-25 04:49:55 -0400

    added params

--- src/main/Market.java
+++ src/main/Market.java
@@ -1,5 +1,7 @@
 package main;
 import java.util.HashMap;
+import java.util.Scanner;
+
 
 public class Market {
     public static HashMap<String, Stock> stocks;
@@ -7,20 +9,44 @@ public class Market {
 
     public static void main(String[] args) {
         // main program to run
+        
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+
     }
 
+    /**
+     * Return all stocks
+     * 
+     * @return   A hash map of every stock 
+     */
     public HashMap<String, Stock> getAllStocks() {
         return stocks;
     }
 
+    /**
+     * Verify stock exists
+     * 
+     * @param   sts Stock ticker symbol
+     * 
+     * @return   Boolean value corresponding to if the stock exists
+     */
     public static boolean verifyStock(String sts) {
         return stocks.containsKey(sts);
     }
 
+    /**
+     * Exit program with success status
+     */
     public void end() {
         System.exit(0);
     }
 
+    /**
+     * Print manual page with list of functions
+     */
     public void manpage() {
         System.out.println("manpage");
     }
